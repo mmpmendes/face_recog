b511ee4bd20291d5885801fd1a3a39101872e2d3
mmpmendes <mendez.marcio@gmail.com>
2018-09-28 11:34:48 +0000

    final ration adjustments

--- script.py
+++ script.py
@@ -39,24 +39,21 @@ def treat_image(file_name, file_dir):
                 face_width = right-left
                 face_height = bottom-top
 
-                min_top = top - face_height*0.2
-                max_bottom = bottom + face_height*0.8
+                min_top = max(top - face_height*0.2, 0)
+                max_bottom = min(bottom + face_height, original_pil_img.height)
 
-                min_left = left - face_width*1.05
-                max_right = right + face_width
+                min_left = max(left - face_width*1.05, 0)
+                max_right = min(right + face_width, original_pil_img.width)
 
                 final_width = original_pil_img.width
                 final_height = original_pil_img.height
 
-                if original_pil_img.width > original_pil_img.height:
+                if original_pil_img.width >= original_pil_img.height:
                     # caso a foto esteja em landscape
                     # fixa a largura
                     # calcula a altura
                     final_height = max_bottom - min_top
                     final_width = final_height * 115/150
-                    if final_height > original_pil_img.height:
-                        final_height = original_pil_img.height
-                        final_width = final_height * 115/150
                     
                 else:
                     # caso a foto esteja em portrait
@@ -64,31 +61,39 @@ def treat_image(file_name, file_dir):
                     # calcula a largura consoante o ASPECT RATIO
                     final_width = max_right - min_left
                     final_height = final_width * 150/115
-                    if final_height > original_pil_img.height:
-                        final_height = original_pil_img.height
-                        final_width = final_height * 115/150
                     
                 # crop imagem no formato correto
                 # centro da cara esta em ((right-left)/2, (bottom-top)/2)
                 center_x = min_left + (max_right-min_left)/2
-                x_min = int(max(center_x - final_width/2, 0))
-                x_max = int(min(center_x + final_width/2, original_pil_img.width))
+                x_min = int(center_x - final_width/2)
+                x_max = int(center_x + final_width/2)
 
                 center_y = min_top + (bottom-min_top)/2
-                y_min = int(max(center_y - final_height/2, 0))
-                y_max = int(min(center_y + final_height/2, original_pil_img.height))
+                y_min = int(center_y - final_height/2)
+                y_max = int(center_y + final_height/2)
+
+                if y_min < 0:
+                    aux_y = y_min
+                    y_min = 0
+                    y_max = y_max - aux_y
+                
 
                 face_image = image[y_min:y_max, x_min:x_max]
                 pil_image = Image.fromarray(face_image)
                 if( n > 0):
-                    print("{}/{}_{}{}".format(results_dir, os.path.splitext(os.path.basename(file_name))[0], n, os.path.splitext(os.path.basename(file_name))[1]))
+                    print("{}/{}_{}{}".format(results_dir, os.path.splitext(os.path.basename(file_name))[0], 
+                    n, os.path.splitext(os.path.basename(file_name))[1]))
                 else:
                     print("{}/{}".format(results_dir, os.path.basename(file_name)))
                 n = n + 1
 
                 final_size = int(final_width), int(final_height)
 
-                pil_image.resize(final_size)
+                # print(x_min, x_max, y_min, y_max)
+                print("{}x{}".format(int(final_width), int(final_height)))
+                # print(final_width/final_height)
+                print("{}x{}".format(x_max-x_min, y_max-y_min))
+                print((x_max-x_min)/(y_max-y_min))
                 pil_image.thumbnail(final_size)
                 pil_image.save(results_dir+"/"+os.path.basename(file_name))
                 
